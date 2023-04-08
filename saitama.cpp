1ee55e21052b521b3bbd2cebc8f6b3274ffed21f
Saransh Raina <saranshraina1@gmail.com>
2023-04-08 15:35:46 -0500

    stuck on reoccurance

--- saitama.cpp
+++ saitama.cpp
@@ -4,33 +4,41 @@
 using namespace std;
 vector<pair<int,int>> lanterns;
 
+int solver() {
+  int counter = 0;
+  int cur_pos = lanterns[lanterns.size() - 1].first;
+  int cur_pow = lanterns[lanterns.size() - 1].second;
+  for (int j = (lanterns.size() / 2) - 2; j > -1; j--) {
+    int check_pos = lanterns[j].first;
+    if (cur_pos - cur_pow > check_pos) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
 int main() {
   int num_lant;
   cin >> num_lant;
   lanterns.resize(num_lant);
   for (int i = 0; i < num_lant; i++) {
-    int cur_power;
     int cur_pos;
-    cin >> cur_power >> cur_pos;
-    pair<int,int> pair = make_pair(cur_power, cur_pos);
+    int cur_power;
+    cin >> cur_pos >> cur_power;
+    pair<int,int> pair = make_pair(cur_pos, cur_power);
     lanterns.push_back(pair);
   }
 
   int min_destroyed = num_lant;
-  for (int i = 0; i < num_lant; i++) {
-    int cur_destroyed = solver();
-    if (min_destroyed > cur_destroyed) {
-      min_destroyed = cur_destroyed;
+  int pos = 0;
+  while (pos < num_lant) {
+    int cur_left = solver();
+    cout << cur_left << "\n";
+    if (min_destroyed > num_lant - cur_left) {
+      min_destroyed = num_lant - cur_left;
     }
+    lanterns.pop_back();
+    pos++;
   }
   cout << min_destroyed;
-}
-
-int solver() {
-  int counter = 0;
-  for (int i = lanterns.size() - 1; i > -1; i--) {
-    int cur_pos = lanterns[i].first;
-    int cur_pow = lanterns[i].second;
-    
-  }
 }

f439e6f4f511407158b043c2b0331ac1975a75d1
Saransh Raina <saranshraina1@gmail.com>
2023-04-14 17:08:46 -0500

    Reverse engineering

--- TwoButtons.java
+++ TwoButtons.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class TwoButtons {
-  
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int start = scanner.nextInt();
+		int end = scanner.nextInt();
+		scanner.close();
+		if (start >= end) {
+			System.out.println(start-end);
+		} else {
+			int steps = 0;
+			while (end > start) {
+				if (end % 2 != 0) {
+					end += 1;
+				} else {
+					end /= 2;
+				}
+				steps += 1;
+			}
+			System.out.println(steps + start-end);
+		}
+		
+	}
+
 }

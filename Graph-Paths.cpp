702e06a0d193f0751721f36c38d90121911ce64f
Saransh Raina <saranshraina1@gmail.com>
2023-04-20 15:34:29 -0500

    graph paths done

--- Graph-Paths.cpp
+++ Graph-Paths.cpp
@@ -8,7 +8,30 @@ long numCity;
 vector<vector<pair<long,long>>> graph(200000, vector<pair<long,long>>());
  
 void solver() {
-  
+  vector<long> output(numCity + 1, LONG_MAX);
+	priority_queue<pair<long,long>, vector<pair<long,long>>, greater<pair<long,long>>> queue;
+	queue.push(make_pair(0,1));
+
+	while (!queue.empty()) {
+		long curlen = queue.top().first; 
+		long curdest = queue.top().second; 
+		queue.pop();
+		if (curlen <= output[curdest]) {
+      for (pair<long,long> curFlight : graph[curdest]) {
+        long oldLen = curFlight.first;
+        long nextDest = curFlight.second;  
+        if (oldLen + curlen < output[nextDest]) {
+          output[nextDest] = oldLen + curlen;
+          queue.push(make_pair(oldLen + curlen, nextDest));
+        }
+      }
+    }
+	}
+  output[1] = 0;
+  for (int i = 0; i < numCity; i++) {
+		cout << output[i + 1];
+    cout << " ";
+	}
 }
  
 int main() {

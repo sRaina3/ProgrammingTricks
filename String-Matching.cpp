855b2791b68409e05a0884af9502b2318e205c4b
Saransh Raina <saranshraina1@gmail.com>
2023-04-20 22:12:26 -0500

    Updated string matching using KMP

--- String-Matching.cpp
+++ String-Matching.cpp
@@ -9,17 +9,30 @@ int main() {
   string s1, s2;
   cin >> s1 >> s2;
   int ans = 0;
-  for (int i = 0; i < s1.length() - s2.length() + 1; i++) {
-    bool works = true;
-    if (s1[i] == s2[0]) {
-      for (int j = i; j < i + s2.length(); j++) {
-        if (s1[j] != s2[j - i]) {
-          works = false;
-        }
-      }
-      if (works) {
-        ans++;
-      }
+  int KMPman[s2.length()] = {0};
+
+  int j = 0;
+  for (int i = 1; i < s2.length(); ++i) {
+    while (j > 0 && s2[i] != s2[j]) {
+      j = KMPman[j - 1];
+    }
+    if (s2[i] == s2[j]) {
+      j++;
+    }
+    KMPman[i] = j;
+  }
+
+  int k  = 0;
+  for (int i = 0; i < s1.length(); ++i) {
+    while (k > 0 && s1[i] != s2[k]) {
+      k = KMPman[k - 1];
+    }
+    if (s1[i] == s2[k]) {
+      k++;
+    }
+    if (k == s2.length()) {
+      k = KMPman[k - 1];
+      ans++;
     }
   }
   cout << ans;
